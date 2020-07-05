6bccad6c608341c56901588a7ed0e6605a60a5d2
unknown <saqlainch92786@gmail.com>
2020-07-06 02:52:43 +0500

    make it better

---
+++ client/build/precache-manifest.553db3a0ec8aa20e7c0f614e573aa74b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1354e784fa5b43effb2f04d65ad7c1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb4906f5f69c28f972fa",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "d72e7bda63a96ae274d8",
+    "url": "/static/css/main.9741d9a1.chunk.css"
+  },
+  {
+    "revision": "fb4906f5f69c28f972fa",
+    "url": "/static/js/2.c6198824.chunk.js"
+  },
+  {
+    "revision": "6a4ea187e5f6da24a1bfc6f7d1d270ed",
+    "url": "/static/js/2.c6198824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d72e7bda63a96ae274d8",
+    "url": "/static/js/main.198b7ad3.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "3f5994228ef8dc0f1bda6c7c98298423",
+    "url": "/static/media/banner_01.3f599422.png"
+  },
+  {
+    "revision": "46b57b89cdc0a254d2953c5e31535fd7",
+    "url": "/static/media/breadcump-img.46b57b89.png"
+  },
+  {
+    "revision": "b98b0888023a0ff87123aeaf5a15ef20",
+    "url": "/static/media/logo1.b98b0888.png"
+  },
+  {
+    "revision": "9dd4a6fb6fedada1dcb783efb128d565",
+    "url": "/static/media/logo2.9dd4a6fb.png"
+  },
+  {
+    "revision": "209a5afefe44443daf554ecb79bf149e",
+    "url": "/static/media/pc.209a5afe.png"
+  },
+  {
+    "revision": "8d2d36e704c87b6b6628961d0ca25ef3",
+    "url": "/static/media/section-bg-6.8d2d36e7.png"
+  },
+  {
+    "revision": "7dcb1c3b6b21a6cb94ef7f8b40d70206",
+    "url": "/static/media/shape-right-2.7dcb1c3b.png"
+  },
+  {
+    "revision": "c856e2f371a283c20b7f86113282dc77",
+    "url": "/static/media/visamaster.c856e2f3.JPG"
+  }
+]);
